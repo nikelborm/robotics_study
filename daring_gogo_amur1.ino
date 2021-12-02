7187a251831c85b206c39b2c702c00b419d372c9
nikelborm <kolya007.klass@gmail.com>
2021-12-02 22:14:20 +0300

    reading analog pins lesson

--- daring_gogo_amur1.ino
+++ daring_gogo_amur1.ino
@@ -1,9 +1,10 @@
+#define __AVR_ATmega2560__
+
 #include <avr/io.h>
 #include <avr/common.h>
 #include <avr/interrupt.h>
 
 #define F_CPU 16000000UL
-#define __AVR_ATmega2560__
 
 
 // void setup() {
@@ -18,6 +19,28 @@
 //   int adc2 = analogRead(A1);
 //   Serial.println(adc2);
 //   delay(1000);
+// }
+
+// int main()
+// {
+//   // записываем 1 на pb5, то есть устанавливаем на выход (светодиод)
+//   DDRB |= 1 << PB5;
+
+//   // записываем 0 на pb2, то есть устанавливаем на выход (кнопка)
+//   DDRB &= ~(1 << PB4); // !00100000 == 11011111
+//   // 00100010 & 11011111 = 00000010
+
+//   // включаем подтяжку к питанию (pull-up)
+//   PORTB |= 1 << PB4;
+
+//   while (1)
+//   {
+//     if (PINB & (1 << PB4))
+//       PORTB &= ~(1 << PB5);
+//     else
+//       PORTB |= 1 << PB5;
+//     _delay_ms(500);
+//   }
 // }
 
 // int main() {
@@ -51,17 +74,51 @@
 //   while (true) {}
 // }
 
-
 // DDRB - 0-вход 1-выход. Data Direction Register
 // PORTB - В режиме выхода: Для установки выходного напряжения 0-low, 1-high.
 // PORTB - В режиме входа:  Для подключения pull-up подтяжки 0-disabled, 1-enabled.
 // PINb - для чтения напряжения
 
+int main()
+{
+  // запускаем АЦП
+  ADCSRA = 1 << ADEN;
+
+  // тактовую частоту микроконтроллера делим на 128, частота АЦП 125 Кгц
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+  // Установили опорное напряжение
+  ADMUX = 1 << REFS0;
+
+  Serial.begin(115200);
+
+  while (true)
+  {
+    // помним о том что MUX5 нужно записывать в ADCSRA, а все остальные в ADMUX
+    // записываем 1 в бит для выбора аналогового порта ADC1
+    ADMUX |= 1 << MUX0;
+
+    // записываем 1 в бит для запуска преобразования
+    ADCSRA |= (1 << ADSC);
+
+    while (ADCSRA & (1 << ADSC)) {}
+
+    // альтернативный вариант написания, но он медленнее Serial.println(ADCL | (ADCH << 8));
+    Serial.print("ADC1: ");
+    Serial.println(ADCW);
 
 
-int main() {
-  
 
-  
-  while (true) {}
+
+    // записываем 0 в бит для выбора аналогового порта ADC0
+    ADMUX &= ~(1 << MUX0);
+
+    // записываем 1 в бит для запуска преобразования
+    ADCSRA |= (1 << ADSC);
+
+    while (ADCSRA & (1 << ADSC)) {}
+
+    Serial.print("ADC0: ");
+    Serial.println(ADCW);
+  }
 }
